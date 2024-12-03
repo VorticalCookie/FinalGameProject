543dd5c3b2eaebb9afdbdb6d6b44d4dbeda55a32
VorticalCookie <92548003+VorticalCookie@users.noreply.github.com>
2024-12-03 08:45:45 -0800

    Player Created
    Player Created

--- GameProject/Assets/Scripts/PlayerMovement.cs
+++ GameProject/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/*
+ * Author: [Espinoza, Marco]
+ * Last Updated: [09/22/2001]
+ * [Handles the player movement/jump force]
+ */
+
 public class PlayerMovement : MonoBehaviour
 {
+
+
+
+    public float Speed = 5f; // Speed of forward movement
+    public float jumpForce = 10f;  // Force applied to ascend
+    public float maxJumpSpeed = 7f; // Max upward velocity
+    public float gravity = 10f;    // Custom gravity force
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    private void Movement()
+    {
+        // Ascend when input is detected
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (rb.velocity.y < maxJumpSpeed)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y + jumpForce * Time.deltaTime, rb.velocity.z);
+            }
+        }
+        else
+        {
+            // Apply custom gravity when not pressing ascend
+            if (rb.velocity.y > -maxJumpSpeed)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y - gravity * Time.deltaTime, rb.velocity.z);
+            }
+        }
     }
 }
