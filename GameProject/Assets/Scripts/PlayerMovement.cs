9829fa1c82c581d55f209bb93c17f0fc0467ac95
VorticalCookie <92548003+VorticalCookie@users.noreply.github.com>
2024-12-11 16:53:23 -0800

    UI updated

--- GameProject/Assets/Scripts/PlayerMovement.cs
+++ GameProject/Assets/Scripts/PlayerMovement.cs
@@ -39,7 +39,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Movement();
-        Debug.Log("Player X Position: " + transform.position.x);
+       // Debug.Log("Player X Position: " + transform.position.x);
     }
 
     private void Movement()
