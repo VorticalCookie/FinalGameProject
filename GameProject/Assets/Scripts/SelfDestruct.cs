3531edcae7bb05db464551f0fda9a4b7d80231c1
VorticalCookie <92548003+VorticalCookie@users.noreply.github.com>
2024-12-11 16:45:07 -0800

    Health Pack Created//Self Destruct updated
    Self destruct now happens every 10 seconds

--- GameProject/Assets/Scripts/SelfDestruct.cs
+++ GameProject/Assets/Scripts/SelfDestruct.cs
@@ -7,7 +7,7 @@ public class SelfDestruct : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 5f);
+        Destroy(gameObject, 10f);
     }
 
     // Update is called once per frame
